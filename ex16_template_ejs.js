6ab55fd7b8a1cbc1c82ab64e3c510cf919aeef73
yodfhafx <yodfha2014@gmail.com>
2019-02-17 12:12:56 +0700

    setup ejs template

---
+++ ex16_template_ejs.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './public');
+
+
+app.get('/', (req, res) => {
+    res.render('index2', {
+        header: 'Ejs Template engine',
+        description: 'Learn NodeJS template engine',
+        books: ['Harry Porter', 'NodeJS', 'PHP', 'Java', 'SCSS']
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server run at port 3000');
+});
